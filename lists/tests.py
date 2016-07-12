61febca9b0290aea0664521eed7d709a6237f030
James Burke <james.burke1203@gmail.com>
2016-07-12 08:12:21 +0200

    new url view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,21 @@ from django.http import HttpRequest
 from lists.views import home_page
 from lists.models import Item
 
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
 class ItemModelTest(TestCase):
 
     def test_home_page_only_saves_items_when_necessary(self):
@@ -49,7 +64,7 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -62,12 +77,3 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode()) 
